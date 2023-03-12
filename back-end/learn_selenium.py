d53735342f38bb833067b8154c737ee0a3079fda
Yuqian-Cao <laineyyangli@gmail.com>
2023-03-12 11:14:01 -0500

    updated learn_selenium.py with an example

--- back-end/learn_selenium.py
+++ back-end/learn_selenium.py
@@ -1,36 +1,58 @@
 import csv
-#from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 from selenium.webdriver.chrome.service import Service
-from bs4 import BeautifulSoup
-import requests
-'''
-def init(s):
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser.get('https://www.heb.com/')
-    # Get cookies for request session
-    cookies = browser.get_cookies()
-    browser.quit()
-    # Creating a session
-    s.session = requests.Session()
-     # PLace cookies in the session cookie jar
-    [requests.Session().cookies.set(cookie['name'], cookie['value']) for cookie in cookies]
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def getLinks(s, product):
-    url = "https://www.heb.com/search/?q=" + product
-    page = s.session.get(url)
-    print(page)
-    #soup = BeautifulSoup(page.text, 'html.parser')
-'''
-#init(s)
+import time
+
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()) )
-driver.get("https://www.heb.com/")
+driver.get("https://www.tamu.edu/")
 
-# this is the amount of time the website will show up 
-time.sleep(5)
+# print out the title of the page
+print(driver.title)
 
-#soup = BeautifulSoup(driver.page_source,features="html.parser")
-#getLinks("bread")
+# ID -> Class -> name
+search = driver.find_element(By.ID, "searchfield")
+# clears the field so we are not appending to pre-existant text
+search.clear()
+search.send_keys("aggie ring")
+search.send_keys(Keys.RETURN)
+
+try:
+    # explicit wait
+    # wait up to 10 seconds until the presence of linker text "Aggie Ring" is found
+    # purpose is to ensure the locating of elements in situations of delays of website loading
+    link = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Aggie Ring"))
+    )
+    link.click()
+    
+    # allows time to pause for 5 seconds, extending the pop up time of the chrome tab
+    time.sleep(5)
+    print(driver.title) 
+
+    element = driver.find_element(By.ID, "ctl00_ContentPlaceHolder1__editor__pageContent")
+    #print(element.text)
+
+    paragraphs = element.find_elements(By.TAG_NAME, 'p')
+    for p in paragraphs[:5]:
+        print(p.text)
+        print()
+
+    '''
+    # allows you to go back to the previous page
+    driver.back()
+    driver.back()
+    # allows you to move forward to the page you went back on
+    driver.forward()
+    time.sleep(5)
+    '''
+
+except:
+    driver.quit()
 
