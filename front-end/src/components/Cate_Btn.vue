fe2d98f16e5e11fc1417adbdb05825a8dc9b09f7
wu814 <william14.wu@gmail.com>
2023-03-11 23:24:42 -0600

    created button

---
+++ front-end/src/components/Cate_Btn.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        <li><button class="cate_btn">Grocery</button></li>
+        <li><button class="cate_btn">Dinning</button></li>
+        <li><button class="cate_btn">Drinks</button></li>
+        <li><button class="cate_btn">Gas</button></li>
+    </ul>  
+</template>
+
+<style>
+    ul {
+        list-style-type: none;
+    }
+    .cate_btn{
+        background-color: #c1b599;
+        color: black;
+        text-decoration: none;
+        border: 2px solid transparent;
+        font-size: 25px;
+        padding: 20px 25px;
+        margin: 15px 0px;
+        border-radius: 15px;
+        width: 250px;
+        transition: .4s;
+    }
+    .cate_btn:hover{
+        transform: scale(1.1);
+    }
+</style>
