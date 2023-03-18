dddede061f238547d71dd2ea1c18e48afca64052
timothyk31 <timothykurniawan16@gmail.com>
2023-03-17 22:13:05 -0500

    Logo file, added picture and style

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Logo from './components/logo.vue'
 </script>
 
 <template>
@@ -9,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it :>" />
-
+      <Logo/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- front-end/src/components/logo.vue
+++ front-end/src/components/logo.vue
@@ -1,3 +1,16 @@
 <template>
-    
+    <div class="logo-wrapper">
+        <div class="logo-picture">
+            <img src="@/assets/broke-logo.png" alt="logo">
+        </div>
+    </div>
 </template>
+
+
+<style>
+.logo-picture{
+    border-radius: 50%;
+    height: 163px;
+    width: 169px;
+}
+</style>
