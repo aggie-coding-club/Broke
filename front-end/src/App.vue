7a19cbcb4f1e09e332b8d79622b66f2aa023e44a
timothyk31 <timothykurniawan16@gmail.com>
2023-03-17 22:32:47 -0500

    Final logo and text

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -9,8 +9,8 @@ import Logo from './components/logo.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it :>" />
       <Logo/>
+      <HelloWorld msg="You did it :>" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
