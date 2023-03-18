48d6dafc95b399a574f1bef64a18c39b2b4c728d
timothyk31 <timothykurniawan16@gmail.com>
2023-03-17 22:18:59 -0500

    logo text

--- front-end/src/components/logo.vue
+++ front-end/src/components/logo.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="logo-wrapper">
-            <img src="@/assets/broke-logo.png" alt="logo" class="logo-picture">
+        <img src="@/assets/broke-logo.png" alt="logo" class="logo-picture">
+        <div class="broke-text">
+            <h2>Broke</h2>
+        </div>
     </div>
 </template>
 
 
 <style>
+
+
 .logo-picture{
     border-radius: 50%;
     height: 163px;
     width: 169px;
+}
+.broke-text{
+    height: 94px;
+    width: 284px;
 }
 
 </style>
