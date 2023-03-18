ebf38270b2226bb606b214292b93416f1337f88a
timothyk31 <timothykurniawan16@gmail.com>
2023-03-17 22:24:27 -0500

    logo arrangement

--- front-end/src/components/logo.vue
+++ front-end/src/components/logo.vue
@@ -1,24 +1,32 @@
 <template>
     <div class="logo-wrapper">
-        <img src="@/assets/broke-logo.png" alt="logo" class="logo-picture">
         <div class="broke-text">
             <h2>Broke</h2>
         </div>
+        <img src="@/assets/broke-logo.png" alt="logo" class="logo-picture">
     </div>
 </template>
 
 
 <style>
-
+.logo-wrapper{
+    height: 163px;
+    display: flex;
+    flex-wrap: wrap;
+}
 
 .logo-picture{
     border-radius: 50%;
     height: 163px;
     width: 169px;
+    margin: 0 auto;
 }
 .broke-text{
     height: 94px;
     width: 284px;
+    display: flex;
+    align-items: center;
+
 }
 
 </style>
