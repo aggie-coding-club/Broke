318c314f9f96e5533ee8c7dcc707ab5d394ed808
salembsaleh <113403270+salembsaleh@users.noreply.github.com>
2023-03-24 05:36:06 -0500

    Update Description.vue

--- front-end/src/components/Description.vue
+++ front-end/src/components/Description.vue
@@ -1,6 +1,11 @@
 <template>
         <div class="description">
-            <h2>Welcome to our website, the ultimate guide to finding the cheapest groceries, food, drinks, and gas in your area! Our website is designed to help you save money on everyday essentials. We understand how important it is to make the most of your hard-earned cash. With just a few clicks, you can compare prices from nearby stores and gas stations to find the best deals in town. 
+            <h2>
+                <br>Welcome to our website, the ultimate guide to finding the cheapest groceries, food, drinks, and gas in your area! 
+                
+                <br>
+                <br>
+                Our website is designed to help you save money on everyday essentials. We understand how important it is to make the most of your hard-earned cash. With just a few clicks, you can compare prices from nearby stores and gas stations to find the best deals in town. 
                 <br>
                 <br>Whether you're looking to stock up on groceries for the week, fill up your gas tank, or grab a quick bite to eat, we've got you covered. Our user-friendly interface makes it easy to search for the products and services you need, and our filters allow you to narrow down your search based on price, distance, and more. 
                 <br>
@@ -12,13 +17,7 @@
 
 <style>
 .description{
-    height: 8vh;
-    width: 20vw;
-    left: 1vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 3vw;
+    font-size: 12px;
     color: #000000;
 }
 
