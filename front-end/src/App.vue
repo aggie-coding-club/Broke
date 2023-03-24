9990c4a87899c5b0225ce3be484d98b1472f14c5
ariana cavazos <arianacavazos52@gmail.com>
2023-03-24 16:28:52 -0500

    added footer

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it :>" />
+      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -18,7 +18,14 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+  <footer>
+    <div>
+      <h3>Contact us at brokeapp@gmail.com</h3>
+      <h3>Feedback </h3>
+    </div>
+  </footer>
 </template>
+
 
 <style scoped>
 header {
@@ -80,6 +87,10 @@ nav a:first-of-type {
 
     padding: 1rem 0;
     margin-top: 1rem;
+  }
+  
+  footer {
+    text-align: center;
   }
 }
 </style>
