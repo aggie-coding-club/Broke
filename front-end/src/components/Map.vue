8ddb5e1d65866eac923f6f5b12120e10644d61d3
ryan tran <113406848+rryantran@users.noreply.github.com>
2023-03-29 19:13:19 -0500

    add map component to result view

--- front-end/src/components/Map.vue
+++ front-end/src/components/Map.vue
@@ -1,18 +1,23 @@
 <template>
-    <div class="map-wrapper">
-        <img src="@/assets/map.png" alt="placeholder image for map" class="map-img">
-    </div>
+  <div class="map-wrapper">
+    <img
+      src="@/assets/map.png"
+      alt="placeholder image for map"
+      class="map-img"
+    />
+  </div>
 </template>
 
 <style>
 .map-wrapper {
-    width: 590px;
-    height: 540px;
+  width: 590px;
+  height: 540px;
 }
+
 .map-img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 5%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 5%;
 }
 </style>
---
+++ front-end/src/views/ResultView.vue
@@ -0,0 +1,6 @@
+<script setup>
+import Map from "../components/Map.vue";
+</script>
+<template>
+  <Map></Map>
+</template>
