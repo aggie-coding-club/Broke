bdd2d1982090e4b9add2a06e7d7e10299d309cd0
Allen Sun <allensun21527@gmail.com>
2023-04-08 16:41:56 -0500

    Searches for product and address

--- back-end/uberEats.py
+++ back-end/uberEats.py
@@ -1,3 +1,14 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+
+import time
+
 def uberEats(stores : list, item : str, address: str) -> dict:
     '''
     Locate address bar
@@ -10,4 +21,46 @@ def uberEats(stores : list, item : str, address: str) -> dict:
     {"Hopdoddy" : ("cheeseburger", 10.99)}
 
     '''
-    pass
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()) )
+    driver.get("https://www.ubereats.com/")
+    driver.maximize_window()
+
+
+    # Search location and navigate to next page
+    location = driver.find_element(By.ID, "location-typeahead-home-input")
+    location.clear()
+    location.send_keys(address)
+    location.send_keys(Keys.RETURN)
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//*[@id="location-typeahead-home-item-0"]').click()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "search-suggestions-typeahead-input")))
+
+    # Search for items
+    items = driver.find_element(By.ID, "search-suggestions-typeahead-input")
+    items.clear()
+    items.send_keys(item)  
+    items.send_keys(Keys.RETURN) 
+
+    time.sleep(5) 
+
+    scraped_products = {}
+
+    # Get Restaurants
+    restaurants = driver.find_elements(By.XPATH, '//*[@id="main-content"]/div/div/div[2]/div/div[2]/div')
+    for restaurant in restaurants:
+        name = restaurant.find_element(By.XPATH, './div[1]/a/h3')
+        print(name.text)
+        # click on restaurants
+        # find menu and process data
+        # go back and click on the next one
+
+
+    print("Yay")
+    return scraped_products
+
+
+
+address = "125 Spence St, College Station" #Zachry
+item = "burgers"
+uberEats([], item, address)
