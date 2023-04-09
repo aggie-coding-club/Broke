767b12ce5a13044bf25141baee5a6cc638d5eeff
yuqian-cao-19 <caoyuqian520@gmail.com>
2023-04-09 01:26:53 -0500

    comment added

--- back-end/restInApi.py
+++ back-end/restInApi.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify# request allows us to get data from front-end
 from flask_cors import CORS
 
 import sys
@@ -9,15 +9,22 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/restdemo', methods=['GET', 'POST'])
-def grocery_test():
+def rest_test():
+    # GET allows front-end to get data from the back-end
     if request.method == 'GET':
+        # calls function
         result = demo()
         if result != None:
+            # make into json file
             return jsonify(result)
         return {'Failed': 'idk'}
+    
+    # POST allows front-end to send datat to the back-end
     elif request.method == 'POST':
         data = request.get_json()
+        # have to use stderr to print in flask
         print(data, file=sys.stderr)
+        # make into json file
         return jsonify({
             'hello': 'world! (POST request)',
             'your item': data['item'],
